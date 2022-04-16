5f7203407f2415f943bfc91286472f0e7448d9ff
zhiwei1998 <zhiwei1000@qq.com>
2022-04-17 01:16:11 +0800

    添加应用过滤 nping openssh-client openssh-keygen

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,5 @@
 # Add a feed source
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+
+git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
