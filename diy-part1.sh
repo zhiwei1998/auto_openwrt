0969ae66e26333786fb38d79afc291b87d2ee426
zhiwei1998 <zhiwei1000@qq.com>
2022-05-02 15:49:01 +0800

    update

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,5 +19,7 @@ echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 
 git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 
-rm -rf package/lean/luci-theme-argon  
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon  
+rm -rf package/lean/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+
+#git clone https://github.com/sirpdboy/luci-app-advanced package/lean/luci-app-advanced
