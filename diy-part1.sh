c824cd96da46ffc269a93dc66cee633f25446b40
zhiwei1998 <zhiwei1000@qq.com>
2022-05-16 19:59:34 +0800

    添加哪吒监控

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,6 +16,8 @@
 # Add a feed source
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+echo 'src-git nezha https://github.com/Erope/openwrt_nezha' >>feeds.conf.default
+
 
 git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 
