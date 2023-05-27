9bb8d611479e6ca56ae639da92ddcedf20edd7b2
zhiwei1998 <33292396+zhiwei1998@users.noreply.github.com>
2023-05-27 15:17:20 +0800

    Update diy-openwrt.sh

--- diy-openwrt.sh
+++ diy-openwrt.sh
@@ -12,7 +12,7 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/10.10.2.1/g' package/base-files/files/bin/config_generate
-sed -i 's/192.168/10.10/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168/10.10/g' package/base-files/files/bin/config_generate
 
 # Add date version
 export DATE_VERSION=$(date +'%Y-%m-%d-%H%M')
